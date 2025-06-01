67b839ab7be49845855d69ed976a92a034438da8
kai <kaianderson9@gmail.com>
2025-06-01 16:34:35 -0700

    deleted old codebase from branch

--- backend/routes/stripe.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchProducts, processRefund } = require('../controllers/stripeController');
-
-// Define your route
-router.get('/products/search', searchProducts);
-router.post('/refund-request', processRefund);
-
-module.exports = router;
