337bc98714b3ff846944b7484c8c18cc947d1493
kai <kaianderson9@gmail.com>
2025-06-18 12:51:57 -0700

    Rate limiter debugging

--- server/src/routes/contactRoutes.ts
+++ server/src/routes/contactRoutes.ts
@@ -4,6 +4,14 @@ import { postContactForm } from "../controllers/contactController";
 
 const router = express.Router();
 
-router.post("/", emailLimiter, postContactForm);
+router.post(
+  "/",
+  (req, res, next) => {
+    console.log("[IP]", req.ip, req.ips, req.headers["x-forwarded-for"]);
+    next();
+  },
+  emailLimiter,
+  postContactForm
+);
 
 export default router;
