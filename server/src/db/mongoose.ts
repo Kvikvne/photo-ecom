f8fb1aa28580241f444a1778c56e44a64d87e556
kai <kaianderson9@gmail.com>
2025-06-20 14:07:02 -0700

    Seperated DB into dev and prod. Script for creating stripe prices for existing printify products.  Env change for seperate DBs

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -1,29 +1,30 @@
 import mongoose from "mongoose";
 
 export async function connectToMongoDB(): Promise<void> {
-    const uri = process.env.MONGO_URI;
+  const uri = process.env.MONGO_URI;
+  const name = process.env.DB_NAME;
 
-    if (!uri) {
-        throw new Error("MONGO_URI is not defined in .env");
-    }
+  if (!uri) {
+    throw new Error("MONGO_URI is not defined in .env");
+  }
 
-    try {
-        await mongoose.connect(uri, {
-            dbName: "kvikvne",
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(uri, {
+      dbName: name
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 }
 
 export async function disconnectFromMongoDB(): Promise<void> {
-    try {
-        await mongoose.disconnect();
-        console.log("Disconnected from MongoDB");
-    } catch (error) {
-        console.error("MongoDB disconnect error:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("MongoDB disconnect error:", error);
+    process.exit(1);
+  }
 }
