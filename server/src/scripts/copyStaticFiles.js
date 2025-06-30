eb425ce2a4a5dbb385214f2865ec4f874948bc3b
kai <kaianderson9@gmail.com>
2025-06-30 16:44:47 -0700

    switched to commonJS syntax to get rid of build warning

--- server/src/scripts/copyStaticFiles.js
+++ server/src/scripts/copyStaticFiles.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const srcDir = path.resolve("src/scripts/data");
 const destDir = path.resolve("dist/scripts/data");
