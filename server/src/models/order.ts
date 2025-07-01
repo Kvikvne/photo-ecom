2ca1113aa08a2e6ba387763da6b5c8e484f02ca2
kai <kaianderson9@gmail.com>
2025-06-30 17:10:16 -0700

    set order.error to null to reset the value hopfully

--- server/src/models/order.ts
+++ server/src/models/order.ts
@@ -1,143 +1,143 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
 interface OrderItem {
-    variantId: number;
-    productId: string;
-    quantity: number;
-    priceInCents: number;
-    title?: string;
-    image?: string;
+  variantId: number;
+  productId: string;
+  quantity: number;
+  priceInCents: number;
+  title?: string;
+  image?: string;
 }
 
 interface AddressTo {
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    country: string; // ISO 3166-1 alpha-2 country code (e.g., "US")
-    region: string; // State or province
-    city: string;
-    address1: string;
-    address2?: string; // Optional
-    zip: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  country: string; // ISO 3166-1 alpha-2 country code (e.g., "US")
+  region: string; // State or province
+  city: string;
+  address1: string;
+  address2?: string; // Optional
+  zip: string;
 }
 
 const AddressToSchema = new Schema<AddressTo>(
-    {
-        first_name: { type: String, required: true },
-        last_name: { type: String, required: true },
-        email: { type: String, required: true },
-        phone: { type: String, required: false },
-        country: { type: String, required: true },
-        region: { type: String, required: true },
-        city: { type: String, required: true },
-        address1: { type: String, required: true },
-        address2: { type: String, required: false },
-        zip: { type: String, required: true },
-    },
-    { _id: false }
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: false },
+    country: { type: String, required: true },
+    region: { type: String, required: true },
+    city: { type: String, required: true },
+    address1: { type: String, required: true },
+    address2: { type: String, required: false },
+    zip: { type: String, required: true }
+  },
+  { _id: false }
 );
 
 const OrderItemSchema = new Schema<OrderItem>(
-    {
-        variantId: { type: Number, required: true },
-        productId: { type: String, required: true },
-        quantity: { type: Number, required: true },
-        priceInCents: { type: Number, required: true },
-        title: String,
-        image: String,
-    },
-    { _id: false }
+  {
+    variantId: { type: Number, required: true },
+    productId: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    priceInCents: { type: Number, required: true },
+    title: String,
+    image: String
+  },
+  { _id: false }
 );
 
 export interface OrderDocument extends Document {
-    sessionId: string;
-    stripeSessionId: string;
-    email?: string;
-    status:
-        | "pending"
-        | "paid"
-        | "processing"
-        | "confirmed"
-        | "shipped"
-        | "delivered"
-        | "failed"
-        | "canceled";
-    lineItems: OrderItem[];
-    stripeCustomerId?: string;
-    stripePaymentIntentId?: string;
-    createdAt: Date;
-    fulfilledAt?: Date;
-    printifyOrderId: string;
-    error?: string;
-    _id: Types.ObjectId;
-    shippedAt: Date;
-    deliveredAt: Date;
-    trackingUrl: string;
-    addressTo: AddressTo;
-    subtotalInCents?: number;
-    shippingInCents?: number;
-    discountInCents?: number;
-    totalAmountPaidInCents: number;
-    shippingMethod: number;
-    taxInCents: number;
-    printifyStatus: string;
-    printifySyncedAt: Date;
-    stripeRefundId: string;
-    stripeReceipt: string;
+  sessionId: string;
+  stripeSessionId: string;
+  email?: string;
+  status:
+    | "pending"
+    | "paid"
+    | "processing"
+    | "confirmed"
+    | "shipped"
+    | "delivered"
+    | "failed"
+    | "canceled";
+  lineItems: OrderItem[];
+  stripeCustomerId?: string;
+  stripePaymentIntentId?: string;
+  createdAt: Date;
+  fulfilledAt?: Date;
+  printifyOrderId: string;
+  error?: string | null;
+  _id: Types.ObjectId;
+  shippedAt: Date;
+  deliveredAt: Date;
+  trackingUrl: string;
+  addressTo: AddressTo;
+  subtotalInCents?: number;
+  shippingInCents?: number;
+  discountInCents?: number;
+  totalAmountPaidInCents: number;
+  shippingMethod: number;
+  taxInCents: number;
+  printifyStatus: string;
+  printifySyncedAt: Date;
+  stripeRefundId: string;
+  stripeReceipt: string;
 }
 
 const OrderSchema = new Schema<OrderDocument>(
-    {
-        sessionId: { type: String },
-        stripeSessionId: { type: String },
-        email: String,
-        status: {
-            type: String,
-            enum: [
-                "pending",
-                "paid",
-                "processing",
-                "confirmed",
-                "shipped",
-                "delivered",
-                "failed",
-                "canceled",
-            ],
-            default: "pending",
-        },
-        lineItems: [OrderItemSchema],
-        stripeCustomerId: String,
-        stripePaymentIntentId: String,
-        fulfilledAt: Date,
-        printifyOrderId: String,
-        error: String,
-        shippedAt: Date,
-        deliveredAt: Date,
-        trackingUrl: { type: String },
-        addressTo: { type: AddressToSchema, required: true },
-        subtotalInCents: { type: Number },
-        shippingInCents: { type: Number },
-        discountInCents: { type: Number },
-        totalAmountPaidInCents: { type: Number, required: true },
-        shippingMethod: { type: Number },
-        taxInCents: { type: Number },
-        printifyStatus: String,
-        printifySyncedAt: Date,
-        stripeRefundId: String,
-        stripeReceipt: String,
+  {
+    sessionId: { type: String },
+    stripeSessionId: { type: String },
+    email: String,
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "paid",
+        "processing",
+        "confirmed",
+        "shipped",
+        "delivered",
+        "failed",
+        "canceled"
+      ],
+      default: "pending"
     },
-    { timestamps: true }
+    lineItems: [OrderItemSchema],
+    stripeCustomerId: String,
+    stripePaymentIntentId: String,
+    fulfilledAt: Date,
+    printifyOrderId: String,
+    error: String,
+    shippedAt: Date,
+    deliveredAt: Date,
+    trackingUrl: { type: String },
+    addressTo: { type: AddressToSchema, required: true },
+    subtotalInCents: { type: Number },
+    shippingInCents: { type: Number },
+    discountInCents: { type: Number },
+    totalAmountPaidInCents: { type: Number, required: true },
+    shippingMethod: { type: Number },
+    taxInCents: { type: Number },
+    printifyStatus: String,
+    printifySyncedAt: Date,
+    stripeRefundId: String,
+    stripeReceipt: String
+  },
+  { timestamps: true }
 );
 
 OrderSchema.index(
-    { stripeSessionId: 1 },
-    {
-        unique: true,
-        partialFilterExpression: {
-            stripeSessionId: { $exists: true, $ne: null },
-        },
+  { stripeSessionId: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      stripeSessionId: { $exists: true, $ne: null }
     }
+  }
 );
 
 export const Order = mongoose.model<OrderDocument>("Order", OrderSchema);
